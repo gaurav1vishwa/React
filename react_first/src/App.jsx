16183887a830cb31884941c625517a9a15a879ce
Gaurav Vishwakarma <shnivishwakarma1234@gmail.com>
2025-12-13 01:35:42 -0800

    first_this is my fist commit

---
+++ react_first/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+
+
+
+// import React from 'react'
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>hey, you are h1</h1>
+//     </div>
+//   )
+// }
+
+// you can also write like this 
+
+const App = ()=>
+{
+  return (
+    <div>
+      <h1>hey, you are h1</h1>
+    </div>
+  )
+}
+
+
+
+
+export default App
